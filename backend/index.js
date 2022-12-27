b4cdd00da3067113bb3425a18993ae0990ef1863
Facuscar <facuscardillo@gmail.com>
2022-12-27 20:52:12 -0300

    Create user route

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from "dotenv";
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
 dotenv.config();
 
 connectDB();
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 4000;
 
