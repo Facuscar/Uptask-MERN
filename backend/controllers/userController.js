964e7cb602ad6d2a0ab6f9abc04d6fc07c7a252b
Facuscar <facuscardillo@gmail.com>
2022-12-28 20:29:45 -0300

    Prevent duplicated users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,19 @@
 import { User } from "../models/User.js";
 
 export const registerUser = async (req, res) => {
+    const { email } = req.body;
+    if (!email) {
+        const error = new Error('Email is missing or invalid');
+        return res.status(400).json({msg: error.message});
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        const error = new Error('User already registered');
+        return res.status(400).json({ msg: error.message });
+    }
+
     try {
         const user = new User(req.body);
         const saveUser = await user.save();
