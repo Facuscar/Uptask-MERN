05051eb65de2ed0f4ffe7d18d873278f302943fe
Facuscar <facuscardillo@gmail.com>
2022-12-29 20:58:21 -0300

    Hash passwords when creating new user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema({
     name: {
@@ -24,6 +25,13 @@ const userSchema = mongoose.Schema({
     }
 }, {
     timestamps: true,
-})
+});
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 export const User = mongoose.model("User", userSchema)
