23d6ee75d7e4b86773c090fe4e87307409f406ea
Facuscar <facuscardillo@gmail.com>
2023-01-02 23:35:25 -0300

    Create controller and routes for projects

---
+++ backend/controllers/projectController.js
@@ -0,0 +1,31 @@
+export const getProjects = async (req, res) => {
+
+};
+
+export const newProject = async (req, res) => {
+
+};
+
+export const getProject = async (req, res) => {
+
+};
+
+export const editProject = async (req, res) => {
+
+};
+
+export const deleteProject = async (req, res) => {
+
+};
+
+export const addCollaborator = async (req, res) => {
+
+};
+
+export const deleteCollaborator = async (req, res) => {
+
+};
+
+export const getTasks = (req, res) => {
+
+}
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from "dotenv";
 import userRoutes from './routes/userRoutes.js';
+import projectRoutes from './routes/projectRoutes.js';
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ dotenv.config();
 connectDB();
 
 app.use("/api/users", userRoutes);
+app.use("/api/projects", projectRoutes);
 
 const PORT = process.env.PORT || 4000;
 
