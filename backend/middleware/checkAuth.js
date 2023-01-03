3761e887c1a44c0763df97cd1badb86e218abbd4
Facuscar <facuscardillo@gmail.com>
2023-01-02 22:52:34 -0300

    Add middleware to ensure user authentication

--- backend/middleware/checkAuth.js
+++ backend/middleware/checkAuth.js
@@ -1,6 +1,25 @@
-const checkAuth = (req, res, next) => {
-    console.log('first');
-    next();
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+
+const checkAuth = async (req, res, next) => {
+    if (
+        req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const { id } = jwt.verify(token, process.env.JWT_SECRET);
+            
+            req.user = await User.findById(id).select('-password -confirmed -token -createdAt -updatedAt -__v');
+            console.log(req.user);
+
+            return next();
+        } catch (error) {
+            return res.status(404).json({ msg: 'There was an error' });
+        }
+    }
+
+    const error = new Error('Invalid token');
+    res.status(401).json({ msg: error.message })
 }
 
 export default checkAuth;
