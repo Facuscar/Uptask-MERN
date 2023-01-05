bc877e1935fe9121ce0ad8e11057769a75749eee
Facuscar <facuscardillo@gmail.com>
2023-01-04 22:47:20 -0300

    Edit project properties

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -36,7 +36,31 @@ export const getProject = async (req, res) => {
 };
 
 export const editProject = async (req, res) => {
+    const { id } = req.params;
 
+    const project = await Project.findById(id);
+
+    if (!project) {
+        const error = new Error('Not found')
+        return res.status(404).json({ msg: error.message });
+    }
+
+    if (project.creator.toString() !== req.user._id.toString()) {
+        const error = new Error('You dont have access to this project')
+        return res.status(401).json({ msg: error.message });
+    }
+
+    project.name = req.body.name || project.name
+    project.descriptin = req.body.descriptin || project.descriptin
+    project.dueDate = req.body.dueDate || project.dueDate
+    project.client = req.body.client || project.client
+
+    try {
+        const dbProject = await project.save();
+        res.json(dbProject);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const deleteProject = async (req, res) => {
