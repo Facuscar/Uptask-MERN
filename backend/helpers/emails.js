42027891b22ca7b52c2dfcd5c5b6782f02a4f2c1
Facuscar <facuscardillo@gmail.com>
2023-01-13 22:52:21 -0300

    Send email with confirmation link to new users

--- backend/helpers/emails.js
+++ backend/helpers/emails.js
@@ -1,5 +1,28 @@
 import nodemailer from 'nodemailer';
 
-export const registerEmail = (data) => {
-    
+export const registerEmail = async (data) => {
+
+    const { email, name, token } = data;
+
+    const transport = nodemailer.createTransport({
+        host: process.env.MAILTRAP_HOST,
+        port: process.env.MAILTRAP_PORT,
+        auth: {
+          user: process.env.MAILTRAP_USER,
+          pass: process.env.MAILTRAP_PASSWORD,
+        }
+    });
+
+    const info = await transport.sendMail({
+        from: '"UpTask - Project administrator" <accounts@uptask.com>',
+        to: email,
+        subject: "UpTask - Confirm your account",
+        text: "Confirm your account in UpTask",
+        html: `
+            <p>Hi ${name}! To start using UpTask you need to confirm your account.</p>
+            <p>Click the following link.</p>
+            <a href="${process.env.WHITELISTED_URL}/confirm/${token}">Confirm account</a>
+            <p>If you didn't request this, you can just ignore this email.</p>
+        `,
+    })
 }
