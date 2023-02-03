2ccf171dfed51bb973e3d1428c03182b7aec614e
Facuscar <facuscardillo@gmail.com>
2023-02-02 21:30:47 -0300

    Create task modal

--- frontend/src/components/ProjectPage/index.tsx
+++ frontend/src/components/ProjectPage/index.tsx
@@ -20,6 +20,8 @@ const ProjectPage: React.FC<ProjectPageProps> = ({ project }) => {
     const [message, setMessage] = useState<string>('');
     const [error, setError] = useState<boolean>(false);
 
+    const [showModal, setShowModal] = useState<boolean>(false);
+
     const deleteProject = async () => {
         try {
             const token = localStorage.getItem('token');
@@ -65,13 +67,17 @@ const ProjectPage: React.FC<ProjectPageProps> = ({ project }) => {
                     </div>
                 </div>
             </div>
-            <button type="button" className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold text-white text-center bg-sky-400 mt-5 flex gap-2 items-center justify-center">
+            <button 
+                type="button" 
+                className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold text-white text-center bg-sky-400 mt-5 flex gap-2 items-center justify-center"
+                onClick={() => setShowModal(true)}
+            >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
                 New task
             </button>
-            <TaskModal />
+            <TaskModal showModal={showModal} setShowModal={setShowModal} />
         </>
     );
 }
