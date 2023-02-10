a0ac7d192c30867f826296fa09998d1f020ef644
Facuscar <facuscardillo@gmail.com>
2023-02-10 19:16:55 -0300

    Styles depending on task state

--- frontend/src/components/ProjectPage/components/Task.tsx
+++ frontend/src/components/ProjectPage/components/Task.tsx
@@ -25,14 +25,14 @@ const Task: React.FC<TaskProps> = ({ task, setShowModal, setCurrentTask, setShow
     return (
         <div className="border-b p-5 flex justify-between items-center">
             <div>
-                <p className="mb-1 text-xl">{name}</p>
+                <p className={`mb-1 text-xl ${state && 'line-through'}`}>{name}</p>
                 <p className="mb-1 text-sm text-gray-500 uppercase">{description}</p>
                 <p className="mb-1 text-xl">{formatDate(dueDate)}</p>
                 <p className="mb-1 text-gray-600">Priority: {priority}</p>
             </div>
             <div className="flex gap-2">
                 <button className="bg-indigo-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg" onClick={handleClick} >Edit</button>
-                <button>{state ? 'Complete' : 'Completed'}</button>
+                <button className={`${state ? 'bg-lime-600': 'bg-slate-600'} px-4 py-3 text-white uppercase font-bold text-sm rounded-lg`}>{state ? 'Completed' : 'Complete'}</button>
                 <button className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg" onClick={handleDeleteClick}>Delete</button>
             </div>
         </div>
