0834745dbb1779cf73d36d945e3dfe3da3d66247
Facuscar <facuscardillo@gmail.com>
2023-02-13 23:12:51 -0300

    Display contributors info

--- frontend/src/components/ProjectPage/components/Contributor.tsx
+++ frontend/src/components/ProjectPage/components/Contributor.tsx
@@ -5,7 +5,18 @@ type ContributorProps = {
 }
 
 const Contributor: React.FC<ContributorProps> = ({ contributor }) => {
-    return <></>
+    const { name, email }  = contributor;
+    return (
+        <div className="border-b p-5 flex justify-between items-center">
+            <div>
+                <p>{name}</p>
+                <p className="text-sm text-gray-700">{email}</p>
+            </div>
+            <div>
+                <button type="button" className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg">Delete</button>
+            </div>
+        </div>
+    );
 };
 
 export default Contributor;
