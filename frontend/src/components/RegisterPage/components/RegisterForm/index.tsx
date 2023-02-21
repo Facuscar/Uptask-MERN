ff8b91a0ed10b8b369a4e62e544a076a610000a1
Facuscar <facuscardillo@gmail.com>
2023-02-20 22:48:42 -0300

    Refactor RegistarPage components

---
+++ frontend/src/components/RegisterPage/components/RegisterForm/index.tsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import { useState, useRef, FormEvent } from "react";
+
+import Alert from "../../../Atoms/Alert";
+import Input from "../../../Atoms/Form/Input";
+import SubmitButton from "../../../Atoms/Form/SubmitButton";
+
+const RegisterForm: React.FC = () => {
+
+    const nameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const secondPasswordRef = useRef<HTMLInputElement>(null);
+
+    const [showAlert, setShowAlert] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>('');
+    const [error, setError] = useState<boolean>(false);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if ([emailRef.current?.value, nameRef.current?.value, passwordRef.current?.value, secondPasswordRef.current?.value].includes('')) {
+            setMessage('All fields are required')
+            setShowAlert(true);
+            setError(true);
+            return;
+        }
+
+        if (passwordRef.current?.value !== secondPasswordRef.current?.value) {
+            setMessage('The passwords do not match');
+            setShowAlert(true);
+            setError(true);
+            return;
+        }
+
+        if (passwordRef.current && passwordRef.current.value.length < 6) {
+            setMessage('The password must have at least 6 characters');
+            setShowAlert(true);
+            setError(true);
+            return;
+        }
+
+        setShowAlert(false);
+        
+        const createUser = async () => {
+            try {
+                const response = await axios.post<{ msg: string }>(import.meta.env.VITE_API_USERS_URL, { name: nameRef.current?.value, email: emailRef.current?.value, password: passwordRef.current?.value });
+                setShowAlert(true);
+                setMessage(response.data.msg);
+                setError(false);
+                (e.target as HTMLFormElement).reset()
+            } catch (error: any) {
+                setShowAlert(true);
+                setMessage(error.response.data?.msg);
+                setError(true);
+            }
+        }
+
+        createUser();
+    }
+
+    return (
+        <>
+            {showAlert && <Alert message={message} error={error} />}
+
+            <form className="my-10 bg-white shadow rounded-lg p-10" onSubmit={handleSubmit} >
+            <Input type="email" name="Email" placeholder="example@example.com" ref={emailRef} />
+            <Input type="text" name="Name" placeholder="Your name" ref={nameRef} />
+            <Input type="password" name="Password" placeholder="Your password" ref={passwordRef} />
+            <Input type="password" name="Repeat the password" placeholder="Repeat your password" id="second_password" ref={secondPasswordRef} />
+
+            <SubmitButton value="Create your account" />
+        </form>
+        </>
+        
+    );
+}
+
+export default RegisterForm;
--- frontend/src/components/RegisterPage/index.tsx
+++ frontend/src/components/RegisterPage/index.tsx
@@ -1,81 +1,17 @@
-import axios from "axios";
-import { useState, useRef, FormEvent } from "react";
-import { Link } from "react-router-dom";
+import RegisterForm from "./components/RegisterForm";
 
-import Alert from "../Atoms/Alert";
-import Input from "../Atoms/Form/Input";
-import SubmitButton from "../Atoms/Form/SubmitButton";
 import TitleWithSpan from "../Atoms/TitleWithSpan";
 import AuthNav from "../AuthNav";
 
 import { PATH } from "../../constants/path";
 
 const RegisterPage: React.FC = () => {
-    const nameRef = useRef<HTMLInputElement>(null);
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const secondPasswordRef = useRef<HTMLInputElement>(null);
-
-    const [showAlert, setShowAlert] = useState<boolean>(false);
-    const [message, setMessage] = useState<string>('');
-    const [error, setError] = useState<boolean>(false);
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if ([emailRef.current?.value, nameRef.current?.value, passwordRef.current?.value, secondPasswordRef.current?.value].includes('')) {
-            setMessage('All fields are required')
-            setShowAlert(true);
-            setError(true);
-            return;
-        }
-
-        if (passwordRef.current?.value !== secondPasswordRef.current?.value) {
-            setMessage('The passwords do not match');
-            setShowAlert(true);
-            setError(true);
-            return;
-        }
-
-        if (passwordRef.current && passwordRef.current.value.length < 6) {
-            setMessage('The password must have at least 6 characters');
-            setShowAlert(true);
-            setError(true);
-            return;
-        }
-
-        setShowAlert(false);
-        
-        const createUser = async () => {
-            try {
-                const response = await axios.post<{ msg: string }>(import.meta.env.VITE_API_USERS_URL, { name: nameRef.current?.value, email: emailRef.current?.value, password: passwordRef.current?.value });
-                setShowAlert(true);
-                setMessage(response.data.msg);
-                setError(false);
-                (e.target as HTMLFormElement).reset()
-            } catch (error: any) {
-                setShowAlert(true);
-                setMessage(error.response.data?.msg);
-                setError(true);
-            }
-        }
-
-        createUser();
-    }
 
     return (
         <>
             <TitleWithSpan title="Create your account and" spanTitle="projects" />
-
-            {showAlert && <Alert message={message} error={error} />}
-
-            <form className="my-10 bg-white shadow rounded-lg p-10" onSubmit={e => handleSubmit(e)} >
-                <Input type="email" name="Email" placeholder="example@example.com" ref={emailRef} />
-                <Input type="text" name="Name" placeholder="Your name" ref={nameRef} />
-                <Input type="password" name="Password" placeholder="Your password" ref={passwordRef} />
-                <Input type="password" name="Repeat the password" placeholder="Repeat your password" id="second_password" ref={secondPasswordRef} />
-
-                <SubmitButton value="Create your account" />
-            </form>
+            
+            <RegisterForm />
 
             <AuthNav leftText="Sign in" leftTo={PATH.LOGIN} rightText="I forgot my password" rightTo={PATH.FORGOT_PASSWORD} />
         </>
