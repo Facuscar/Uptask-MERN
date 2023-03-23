9f14705ba4fa81e2db302f0b6d8a82106d9ec7ae
Facuscar <facuscardillo@gmail.com>
2023-03-22 22:45:53 -0300

    Hide search list if not in projects page

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 import Button from "components/Atoms/Button";
 import { PATH } from "constants/path";
@@ -12,6 +12,7 @@ import * as S from './styles';
 
 const Header: React.FC = () => {
     const navigate = useNavigate();
+    const location = useLocation();
 
     const { logOut } = useAuth();
     const { clearState } = useProjects();
@@ -23,13 +24,15 @@ const Header: React.FC = () => {
         navigate('/');
     }
 
+    const shouldShowSearch = location.pathname === PATH.PROJECTS;
+
     return (
         <S.MainWrapper>
             <S.Wrapper>
                 <S.Heading>UpTask</S.Heading>
 
                 <S.NavWrapper>
-                    <Search />
+                    {shouldShowSearch && <Search />}
                     <Link to={PATH.PROJECTS} className="font-bold uppercase">Projects</Link>
                     <Button type="button" text="Log out" onClick={handleClick} />
                 </S.NavWrapper>
